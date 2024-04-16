11d0ce35bf5197496ea6d493079325cc9cf37b3f
Samson <babayants.s@gmail.com>
2024-04-16 00:05:15 -0500

    Now it extracts all replies

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
-import os
 import googleapiclient.discovery
 import googleapiclient.errors
 
 from secret_key import *
 
-api_service_name = "youtube"
-api_version = "v3"
+def video_comments(video_id):
+    replies = []
+    youtube = googleapiclient.discovery.build('youtube', 'v3',
+        developerKey = youtube_api_key)
+    video_threads = youtube.commentThreads().list(
+        part = 'snippet',
+        videoId = video_id,
+        maxResults = 5
+    ).execute()
 
-youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, developerKey=youtube_api_key)
+    while video_threads:
+        # get top level comments
+        for thread in video_threads['items']:
+            comment = thread['snippet']['topLevelComment']['snippet']['textDisplay']
+            replycount = thread['snippet']['totalReplyCount']
+            # get replies for a top level comment
+            if replycount > 0:
+                video_thread_replies=youtube.comments().list(
+                    part = 'snippet',
+                    parentId = thread['snippet']['topLevelComment']['id'],
+                    maxResults=5
+                ).execute()
+                while video_thread_replies:
+                    for reply in video_thread_replies['items']:
+                        replies.append(reply['snippet']['textDisplay'])
+                    # check if there is more replies
+                    if 'nextPageToken' in video_thread_replies:
+                        video_thread_replies = youtube.comments().list(
+                            part = 'snippet',
+                            parentId = thread['snippet']['topLevelComment']['id'],
+                            pageToken = video_thread_replies['nextPageToken']
+                        ).execute()
+                    else:
+                        break
+            # print a top level comment with its replies
+            print(comment, replies, replycount, end = '\n\n')
+            replies = []
+        # check if there is more top level comments
+        if 'nextPageToken' in video_threads:
+            video_threads = youtube.commentThreads().list(
+                part = 'snippet',
+                videoId = video_id,
+                pageToken = video_threads['nextPageToken']
+            ).execute()
+        else:
+            break
 
-request = youtube.commentThreads().list(
-    part="snippet",
-    videoId=youtube_video_id,
-    maxResults=100
-)
-response = request.execute()
 
-for item in response['items']:
-    print(item['snippet']['topLevelComment']['snippet']['textDisplay'])
+# Enter video id
+video_id = youtube_video_id
+
+# Call function
+video_comments(video_id)
