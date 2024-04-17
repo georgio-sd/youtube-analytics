e6212512ad552b6c4cfe15cd03e2c393e71ad40a
Samson <babayants.s@gmail.com>
2024-04-17 00:06:39 -0500

    added openAI

--- main.py
+++ main.py
@@ -4,27 +4,37 @@ import re
 
 from secret_key import *
 
+from langchain_openai import ChatOpenAI
+llm = ChatOpenAI(
+    model_name = "gpt-3.5-turbo",
+    temperature = 0.6,
+    max_tokens = 512,
+    api_key = chatgpt_api_key)
+
+answer = llm.invoke("how can langsmith help with testing?")
+print(answer)
+
 def video_comments(video_id):
-    replies = []
+    comments = []
+    topic = []
     youtube = googleapiclient.discovery.build('youtube', 'v3',
         developerKey = youtube_api_key)
     video_threads = youtube.commentThreads().list(
         part = 'snippet',
         videoId = video_id,
-        textFormat = 'plainText'
+        textFormat = 'plainText',
     ).execute()
 
     while video_threads:
         # get top level comments
         for thread in video_threads['items']:
-            comment = re.sub('\n', ' ', thread['snippet']['topLevelComment']['snippet']['textDisplay'])
-            replycount = thread['snippet']['totalReplyCount']
+            topic.append(re.sub('\n', ' ', thread['snippet']['topLevelComment']['snippet']['textDisplay']))
             # get replies of a top level comment
-            if replycount > 0:
+            if thread['snippet']['totalReplyCount'] > 0:
                 video_thread_replies=youtube.comments().list(
                     part = 'snippet',
                     parentId = thread['snippet']['topLevelComment']['id'],
-                    textFormat = 'plainText'
+                    textFormat = 'plainText',
                 ).execute()
                 while video_thread_replies:
                     for reply in video_thread_replies['items']:
@@ -32,9 +42,7 @@ def video_comments(video_id):
                         s = re.sub('[\u200d]', ' ', s)
                         s = re.sub('[\u200b]', ' ', s)
                         s = re.sub('\n', ' ', s)
-                        replies.append(s)
-                        #print(reply['snippet']['textDisplay'])
-                        #print(re.sub('@@[a-zA-Z0-9_-]+', ' ', reply['snippet']['textDisplay']), end = '\n\n')
+                        topic.append(s)
 
                     # check if there is more replies
                     if 'nextPageToken' in video_thread_replies:
@@ -42,27 +50,30 @@ def video_comments(video_id):
                             part = 'snippet',
                             parentId = thread['snippet']['topLevelComment']['id'],
                             textFormat = 'plainText',
-                            pageToken = video_thread_replies['nextPageToken']
+                            pageToken = video_thread_replies['nextPageToken'],
                         ).execute()
                     else:
                         break
             # print a top level comment with its replies
-            print(comment, replies, replycount, end = '\n\n')
-            replies = []
+            #print(topic, end = '\n\n')
+            comments.append(topic)
+            topic = []
         # check if there is more top level comments
         if 'nextPageToken' in video_threads:
             video_threads = youtube.commentThreads().list(
                 part = 'snippet',
                 videoId = video_id,
                 textFormat = 'plainText',
-                pageToken = video_threads['nextPageToken']
+                pageToken = video_threads['nextPageToken'],
             ).execute()
         else:
             break
+    return comments
 
 
 # Enter video id
 video_id = youtube_video_id
 
 # Call function
-video_comments(video_id)
+comments = video_comments(video_id)
+print(comments)
